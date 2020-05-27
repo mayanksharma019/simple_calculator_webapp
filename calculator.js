42730bd6dd4dcb5db109e9b26bfe8ddce433017c
mayank019 <mayanksharma1902@gmail.com>
2020-05-27 12:30:12 +0530

    2nd commit

--- calculator.js
+++ calculator.js
@@ -1,6 +1,10 @@
 const express=require("express");
 const app=express();
+
+
 app.get("/",function(req,res){
-  res.send("hello world");
+  res.sendFile(__dirname+"/index.html");
 });
+
+
 app.listen(3000);
